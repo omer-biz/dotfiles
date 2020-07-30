20827dcddf4634a2010e56554fb8a361935cf03e
omer <omerabdi@protonmail.com>
2020-07-30 15:35:50 +0000

    made make_it_happen a bit modular. Added xbindkeys

--- make_it_happen.sh
+++ make_it_happen.sh
@@ -1,14 +1,32 @@
-#!/usr/bin/sh
+#!/bin/sh
+
+# $0: file
+# $1: dest
+function link() {
+		echo "[*] linking $file ..."
+		ln -sr $1 $2 2>/dev/null
+
+		if [ $? = 0 ]
+	   	then
+				echo "[*] $file linked"
+		else
+				echo "[!] $file exists"
+		fi
+
+		echo
+}
+
 
 # for those who reside in the home directory
-for file in .tmux.conf .vimrc .xinitrc .Xresources
+for file in .tmux.conf .vimrc .xinitrc .Xresources .xbindkeysrc
 do
-	ln -sr $file ~/
-	echo "[*] linking $file ..."
+	# ln -sr $file ~/ 2>/dev/null
+	link $file ~/ 
 done
 
 # fish config
-ln -sr config.fish ~/.config/fish/
+# ln -sr config.fish ~/.config/fish/
+link cnofig.fish ~/.config/fish
 
-echo "[!] Done .."
-
+echo "[#] Done .."
+ 
